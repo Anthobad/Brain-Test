a1fa0b56b1bde05087ae12c86d22cbae2c915272
Anthobad <anthonybadawi100@gmail.com>
2021-08-29 10:27:42 +0300

    My First Commit

---
+++ questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What is the name of the toy cowboy in Toy Story?",
+        answer: "Woody",
+        options: [
+            "Spike",
+            "Woody",
+            "Alex",
+            "Mike"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the color of an emerald?",
+        answer: "Green",
+        options: [
+            "Blue",
+            "Red",
+            "Yellow",
+            "Green"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What fruit do kids traditionally give to teachers?",
+        answer: "An Apple",
+        options: [
+            "An Apple",
+            "A Banana",
+            "A PineApple",
+            "An Orange"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Where does Santa Claus live?",
+        answer: "The North Pole",
+        options: [
+            "The South Pole",
+            "Asia",
+            "Europe",
+            "The North Pole"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many colors are in a rainbow?",
+        answer: "Seven",
+        options: [
+            "Seven",
+            "Six",
+            "Eight",
+            "Five"
+        ]
+    },
+];
