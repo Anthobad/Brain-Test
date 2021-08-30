96dfe6ad311009eebcad459a5d6f0eaef57451a3
Anthobad <anthonybadawi100@gmail.com>
2021-08-30 15:16:58 +0300

    Math

---
+++ Math questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "How many countries are there in the world?",
+        answer: "195",
+        options: [
+            "196",
+            "195",
+            "200",
+            "6"
+        ]
+    },
+    {
+        numb: 2,
+        question: "How many blue stripes are there on the U.S. flag?",
+        answer: "0",
+        options: [
+            "7",
+            "13",
+            "6",
+            "0"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the color of Donald Duck’s bowtie?",
+        answer: "Red",
+        options: [
+            "Red",
+            "Yellow",
+            "White",
+            "Blue"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which planet is the hottest?",
+        answer: "Venus",
+        options: [
+            "Saturn",
+            "Mercury",
+            "Europe",
+            "Venus"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the rarest blood type?",
+        answer: "AB-Negative",
+        options: [
+            "AB-Negative",
+            "O",
+            "A",
+            "B"
+        ]
+    },
+];
