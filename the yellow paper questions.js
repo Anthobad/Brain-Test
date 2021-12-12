e830ff9ab8546c12abaa0a9676ab7f991b49c928
Anthobad <anthonybadawi100@gmail.com>
2021-12-12 17:57:47 +0200

    yellow

---
+++ the yellow paper questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "The story is written from which point of view?",
+        answer: "1st person",
+        options: [
+            "1st person",
+            "3rd person",
+            "3rd person limited",
+            "3rd person omniscient"
+        ]
+    },
+    {
+        numb: 2,
+        question: "To improve the narrator's health, her family decides to...",
+        answer: "change their home",
+        options: [
+            "Eat more vegetables",
+            "Get plenty of exercise",
+            "Pray regularly",
+            "change their home"
+        ]
+    },
+    {
+        numb: 3,
+        question: "This story is ironic because",
+        answer: "Actions are having effects opposite of the intended effects.",
+        options: [
+            "Actions are having effects opposite of the intended effects.",
+            "We know information that the characters in this play lack.",
+            "Sarcasm is used to express the opposite of the literal meaning of the text.",
+            "The overall mood is creepy."
+        ]
+    },
+    {
+        numb: 4,
+        question: "The narrator's husband thinks her health is",
+        answer: "improving over the course of the story",
+        options: [
+            "deteriorating sharply, then improving occasionally",
+            "improving slowly, then deteriorating",
+            "deteriorating over the course of the story",
+            "improving over the course of the story"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Doing nothing all day made the narrator _____ ______",
+        answer: "Extremely anxious",
+        options: [
+            "Extremely anxious",
+            "Very happy",
+            "Extremely tired",
+            "Extremely sad"
+        ]
+    },
+];
